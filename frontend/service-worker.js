fd0e88d1dd9988803ab540e5194b5df707d076f6
hidry313 <hidry313@outlook.sa>
2025-11-28 19:09:53 +0300

    Bump service worker version to v2 to force UI update

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,23 +1,8 @@
-const CACHE_NAME = 'video-downloader-v1';
-const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/app.js',
-    '/manifest.json',
-    '/icons/icon-192.png',
-    '/icons/icon-512.png'
-];
-
-// Install event - cache assets
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
             .then((cache) => {
                 console.log('Caching app assets');
                 return cache.addAll(ASSETS_TO_CACHE);
             })
-            .then(() => self.skipWaiting())
+    .then(() => self.skipWaiting())
     );
 });
 
